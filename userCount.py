bf9fe31489c9e362170688099aa9321051d3c275
Ram-KT <101414392+Ram-KT@users.noreply.github.com>
2024-09-13 12:35:58 +0530

    Update userCount.py

--- userCount.py
+++ userCount.py
@@ -11,12 +11,11 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 # =============================================================================
-# components.html(
-#     """
-#     <iframe src="https://teams.microsoft.com/convene/meetings/b5ff47a6-f7b4-4abf-a484-a75057bd8139__e6de7ade-191f-4b03-96a0-63290e12461e__19:ameeting_ZDlkYjIwNDAtYThiNy00ZDNjLWFkZjItODlhOTkwYjg4ZDQ3@thread.v2/join?embed=true" width='1280' height='720' allowfullscreen"></iframe>
-# 
-#     """,height=720, width=1500 )
-# =============================================================================
+components.html(
+    """
+     <iframe src="https://teams.microsoft.com/convene/meetings/b5ff47a6-f7b4-4abf-a484-a75057bd8139__e6de7ade-191f-4b03-96a0-63290e12461e__19:ameeting_ZDlkYjIwNDAtYThiNy00ZDNjLWFkZjItODlhOTkwYjg4ZDQ3@thread.v2/join?embed=true" width='1280' height='720' allowfullscreen"></iframe>
+     """,height=720, width=1500 )
+ =============================================================================
     
 # =============================================================================
 # components.iframe("https://teams.microsoft.com/convene/meetings/b5ff47a6-f7b4-4abf-a484-a75057bd8139__e6de7ade-191f-4b03-96a0-63290e12461e__19:ameeting_ZDlkYjIwNDAtYThiNy00ZDNjLWFkZjItODlhOTkwYjg4ZDQ3@thread.v2/join?embed=true")    
